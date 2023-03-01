b1836b24e6525bc3159bad48710fe73ed1775fa4
Mico Häsänen <micohasanen@gmail.com>
2023-03-01 17:44:22 +0200

    Switched to lower bitrates

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 export default {
 	bitrates: [
-		{ height: 234, kbit: 145 },
-		{ height: 360, kbit: 365 },
-		{ height: 432, kbit: 1100 },
-		{ height: 480, kbit: 1500 },
-		{ height: 540, kbit: 2000 },
-		{ height: 720, kbit: 4500 },
-		{ height: 1080, kbit: 6000 },
+		{ height: 234, kbit: 350 },
+		{ height: 360, kbit: 500 },
+		{ height: 432, kbit: 800 },
+		{ height: 480, kbit: 1000 },
+		{ height: 540, kbit: 1200 },
+		{ height: 720, kbit: 1800 },
+		{ height: 1080, kbit: 4500 },
 	]
 }
